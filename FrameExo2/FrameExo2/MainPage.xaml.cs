ffa5729b3ebfdc7d9bbb50444f7d06f1c8b797a0
Olivier Couvez <o.couvez@free.fr>
2021-04-09 14:36:24 +0200

    projet sans bin et obj

---
+++ FrameExo2/FrameExo2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace FrameExo2
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+
+        bool OnTimerTick()
+        {
+            _timePicker.Time = DateTime.Now.TimeOfDay;
+            return true;
+        }
+    }
+    
+}
